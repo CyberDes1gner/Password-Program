1f9e878af65bffc5a3656582d44a6b200e14e5da
Cyber <93376222+lolCyberlol@users.noreply.github.com>
2022-09-24 18:53:32 +0100

    Password Program V1.01
    Added username variable

--- Password Program.py
+++ Password Program.py
@@ -1,7 +1,9 @@
 realPassword = 'Password123'
+realUserName = 'Cyber'
+userName = input('Please Enter User Name to Gain Access: ')
 userPassword = input('Please Enter Password To Gain Access: ')
 
-while realPassword != userPassword:
-    password = input('Wrong password. Please Retry: ')
-    if password == 'Password123':
-        print('You Have Been Given Access')
+while realPassword != userPassword and realUserName != userName:
+    userName = input('Wrong Username please retry: ' )
+    userPassword = input('Wrong password. Please Retry: ')
+print('You Have Been Given Access')
